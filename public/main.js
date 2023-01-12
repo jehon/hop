83befe08a0b2f0e651f80c26fdb6aff856d902b5
Jean Honlet <jehon@users.noreply.github.com>
2023-01-12 13:12:13 +0000

    style: lint

--- public/main.js
+++ public/main.js
@@ -5,44 +5,44 @@ const URL = location.search.substr(1);
 
 const logs = document.querySelector('#logs');
 function log(txt) {
-    logs.insertAdjacentHTML('beforeend', `<div>${(new Date()).toTimeString().split(' ')[0]} ${txt}</div>`);
-    logs.scrollTop = logs.scrollHeight;
+  logs.insertAdjacentHTML('beforeend', `<div>${(new Date()).toTimeString().split(' ')[0]} ${txt}</div>`);
+  logs.scrollTop = logs.scrollHeight;
 }
 
-const status = document.querySelector('#status');
+const statusEl = document.querySelector('#status');
 function setStatus(txt) {
-    status.innerHTML = txt;
+  statusEl.innerHTML = txt;
 }
 
 setStatus('loading...');
 
 async function sleep(secs) {
-    return new Promise(resolve => {
-        setTimeout(resolve, secs * 1000);
-    });
+  return new Promise(resolve => {
+    setTimeout(resolve, secs * 1000);
+  });
 }
 
 async function getURL(url) {
-    return fetch(url)
-        .then(response => response.text);
+  return fetch(url)
+    .then(response => response.text);
 }
 
 (async function () {
-    let i = 0;
-    while (i < 5) {
-        log('testing ' + URL);
-        try {
-            setStatus('Trying');
-            await getURL(URL);
-            log('fetching is success');
-            setStatus('Loaded');
-            document.location = URL;
-        } catch (e) {
-            console.warn(e);
-            log('fetching failed');
-        }
-        setStatus('Sleeping');
-        await sleep(1);
-        i++;
+  let i = 0;
+  while (i < 5) {
+    log('testing ' + URL);
+    try {
+      setStatus('Trying');
+      await getURL(URL);
+      log('fetching is success');
+      setStatus('Loaded');
+      document.location = URL;
+    } catch (e) {
+      console.warn(e);
+      log('fetching failed');
     }
+    setStatus('Sleeping');
+    await sleep(1);
+    i++;
+  }
 })();
